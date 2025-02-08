faa2d03cdde02ad8131e86c4cb3859d78a5fe59d
mbank14 <karangvisual@pm.me>
2025-02-08 21:55:56 +0700

    final mobile

--- src/app/(tipcalc)/tip-calc/page.tsx
+++ src/app/(tipcalc)/tip-calc/page.tsx
@@ -3,7 +3,7 @@ import { ChangeEvent, useState } from "react";
 import { formatPrice } from "@/lib/utils/fromatPrice";
 
 const TipCalc = () => {
- const [customPercentage, setCustomPercentage] = useState<number | string>('');
+  const [customPercentage, setCustomPercentage] = useState<number | string>("");
   const [tip, setTip] = useState<number>(142.55);
   const [numberPpl, setNumberPpl] = useState<number>(5);
   const [percentTip, setPercentTip] = useState<number | string>(0.15);
@@ -17,27 +17,27 @@ const TipCalc = () => {
   };
 
   const handleRadio = (e: ChangeEvent<HTMLInputElement>) => {
-    const val = e.target.value
-    if(val !== 'custom' ){
+    const val = e.target.value;
+    if (val !== "custom") {
     }
-    setCustomPercentage('')
+    setCustomPercentage("");
     setPercentTip(Number(e.target.value));
     console.log(val);
   };
 
-  const handleCustomPercentage = (e: ChangeEvent<HTMLInputElement>) =>{
-
-    setCustomPercentage(Number(e.target.value))
-    setPercentTip(Number(e.target.value)/100)
+  const handleCustomPercentage = (e: ChangeEvent<HTMLInputElement>) => {
+    if(Number(e.target.value) == 0) return setCustomPercentage('')
+    setCustomPercentage(Number(e.target.value));
+    setPercentTip(Number(e.target.value) / 100);
     console.log(customPercentage == percentTip);
-  }
+  };
   return (
-    <div>
-      <div className="text-center">
-        <h1>Title</h1>
+    <div className="bg-teal-50">
+      <div className="text-center h-60 flex items-center justify-center">
+        <h1 className="font-bold tracking-wider line-clamp-2">SPLITTER</h1>
       </div>
 
-      <div className="p-5">
+      <div className="p-5 bg-white rounded-t-2xl">
         <div>
           <form action="">
             <div className="mb-4">
@@ -53,13 +53,11 @@ const TipCalc = () => {
                   id="tipAmount"
                   name="tipAmount"
                   value={tip}
-                  onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                  {
+                  onChange={(e: ChangeEvent<HTMLInputElement>) => {
                     const value = Math.max(Number(e.target.value), 1);
-                    e.target.value =  value.toString()  
-                      setTip(Number(value))
-                    }
-                  }
+                    e.target.value = value.toString();
+                    setTip(Number(value));
+                  }}
                   style={{ textAlign: "right" }}
                   onFocus={(e) =>
                     e.target.parentElement?.classList.add("outline-input")
@@ -73,7 +71,7 @@ const TipCalc = () => {
 
             <div className="mb-4">
               <label>Select tip %</label>
-              <div className="grid grid-cols-2 gap-2">
+              <div className="grid grid-cols-2 gap-2 input-percentage">
                 <div>
                   <input
                     onChange={handleRadio}
@@ -81,71 +79,114 @@ const TipCalc = () => {
                     id="tip5"
                     name="tipPercentage"
                     value={0.05}
-                    checked={percentTip == 0.05 && customPercentage == ''}
+                    checked={percentTip == 0.05 && customPercentage == ""}
                   />
-                  <label htmlFor="tip5">5%</label>
+                  <label
+                    className={`${
+                      percentTip == 0.05 && customPercentage == ""
+                        ? "bg-emerald-300 text-emerald-800"
+                        : "bg-emerald-800 text-emerald-300"
+                    }`}
+                    htmlFor="tip5"
+                  >
+                    5%
+                  </label>
                 </div>
 
                 <div>
                   <input
-                    checked={percentTip == 0.10 && customPercentage == ''}
+                    checked={percentTip == 0.1 && customPercentage == ""}
                     type="radio"
                     id="tip10"
                     name="tipPercentage"
-                    value={0.10}
+                    value={0.1}
                     onChange={handleRadio}
                   />
-                  <label htmlFor="tip10">10%</label>
+                  <label
+                    className={`${
+                      percentTip == 0.1 && customPercentage == ""
+                        ? "bg-emerald-300 text-emerald-800"
+                        : "bg-emerald-800 text-emerald-300"
+                    }`}
+                    htmlFor="tip10"
+                  >
+                    10%
+                  </label>
                 </div>
 
                 <div>
                   <input
-                  checked={percentTip == 0.15 && customPercentage == ''}
+                    checked={percentTip == 0.15 && customPercentage == ""}
                     onChange={handleRadio}
                     type="radio"
                     id="tip15"
                     name="tipPercentage"
                     value={0.15}
                   />
-                  <label htmlFor="tip15">15%</label>
+                  <label
+                    className={`${
+                      percentTip == 0.15 && customPercentage == ""
+                        ? "bg-emerald-300 text-emerald-800"
+                        : "bg-emerald-800 text-emerald-300"
+                    }`}
+                    htmlFor="tip15"
+                  >
+                    15%
+                  </label>
                 </div>
 
                 <div>
                   <input
-                  checked={percentTip == 0.20 && customPercentage == ''}
+                    checked={percentTip == 0.2 && customPercentage == ""}
                     onChange={handleRadio}
                     type="radio"
                     id="tip20"
                     name="tipPercentage"
-                    value={0.20}
+                    value={0.2}
                   />
-                  <label htmlFor="tip20">20%</label>
+                  <label
+                    className={`${
+                      percentTip == 0.2 && customPercentage == ""
+                        ? "bg-emerald-300 text-emerald-800"
+                        : "bg-emerald-800 text-emerald-300"
+                    }`}
+                    htmlFor="tip20"
+                  >
+                    20%
+                  </label>
                 </div>
 
                 <div>
                   <input
-                  checked={percentTip == 0.25 && customPercentage == ''}
+                    checked={percentTip == 0.25 && customPercentage == ""}
                     onChange={handleRadio}
                     type="radio"
                     id="tip25"
                     name="tipPercentage"
                     value={0.25}
                   />
-                  <label htmlFor="tip25">25%</label>
+                  <label
+                    className={`${
+                      percentTip == 0.25 && customPercentage == ""
+                        ? "bg-emerald-300 text-emerald-800"
+                        : "bg-emerald-800 text-emerald-300"
+                    }`}
+                    htmlFor="tip25"
+                  >
+                    25%
+                  </label>
                 </div>
 
-                <div>
+                
+                  <input
+                    placeholder="Custom"
+                    type="number"
+                    name="tipPercentage"
+                    id="inputPercentage"
+                    value={customPercentage}
+                    onChange={handleCustomPercentage}
+                  />
 
-                    <input 
-                        placeholder="Custom"
-                        type="number" 
-                        name="tipPercentage" 
-                        id="inputPercentage" 
-                        value={customPercentage}
-                        onChange={handleCustomPercentage}
-                     />
-
-                </div>
               </div>
 
               <input
@@ -170,7 +211,7 @@ const TipCalc = () => {
                   value={numberPpl}
                   onChange={(e: ChangeEvent<HTMLInputElement>) => {
                     const value = Math.max(Number(e.target.value), 1);
-                    e.target.value =  value.toString()
+                    e.target.value = value.toString();
                     setNumberPpl(value);
                   }}
                   onFocus={(e) =>
@@ -214,12 +255,14 @@ const TipCalc = () => {
           </div>
 
           <div>
-            <button onClick={() => {
-                setTip(1)
-                setNumberPpl(1)
-                setPercentTip(0.05)
-            }}
-             className="bg-emerald-300 text-emerald-800 font-bold text-center py-3 block w-full mt-5 rounded-xl">
+            <button
+              onClick={() => {
+                setTip(1);
+                setNumberPpl(1);
+                setPercentTip(0.05);
+              }}
+              className="bg-emerald-300 text-emerald-800 font-bold text-center py-3 block w-full mt-5 rounded-xl"
+            >
               RESET
             </button>
           </div>
